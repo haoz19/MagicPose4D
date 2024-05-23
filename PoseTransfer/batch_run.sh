d122e4118015cfdf1f50435a050634aa4fa8f0a2
Hao Zhang <123277361+haoz19@users.noreply.github.com>
2024-05-22 23:54:43 -0500

    Add files via upload

---
+++ PoseTransfer/batch_run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py bear3EP_attack3
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py bear3EP_Drink
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py bucksYJL_GetHit0
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py bullMJ6_TrotF
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py cattleAFK_Attack2
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py cattleAFK_Attack3
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py cattleAFK_RunR
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py cattleAFK_WalkB
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py deerOMG_TrotforwardRM
+
+
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py deerFEL_drink
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py deerFEL_Attack1
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py deerFEL_Attack2
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py deerFEL_Attack3
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py deerFEL_Jump
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py deerFEL_JumpTrotRM
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py duck_fly
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py rabbit7L6_RunforwardRM
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py deerOMG_JumpTrotRM
+
+
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py bear3EP_Attack4
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py bear3EP_AttackrunRM
+#/scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py doggieMN5_JumpupRM
+#/scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train3.py human1
+
+# l9 = [
+#     '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/medea_DancingTwerk',
+#       '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/lusth_RunningJump', 
+#       '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/lusth_Jumping',
+#     '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/crypto_Punching',
+#     '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/boss_DrunkWalk',
+#     '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/Xbot_HipHopDancing0',
+#     '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/Xbot_HipHopDancing1',
+#     '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/Xbot_HipHopDancing2',
+#     '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/Xbot_HipHopDancing3',
+#     '/u/haozhang/files/4D data/DeformingThings4D/experiments_new/Xbot_HipHopDancing4'
+#     ]
+
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py lusth_RunningJump
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py lusth_Jumping
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py crypto_Punching
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py boss_DrunkWalk
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py james_Shuffling
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py james_WalkingBackwards
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py crypto_Standing2HMagicAttack01
+/scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py Xbot_HipHopDancing
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py Xbot_HipHopDancing4
+# /scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py medea_DancingTwerk
+
+#/scratch/bbqk/haozhang/envs/pytorch3d/bin/python stage2_train4.py human1
